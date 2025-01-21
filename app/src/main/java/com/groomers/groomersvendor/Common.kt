d0689b1dffc1c15490a82ce70feda75f1076e217
SomenathMahata <somenathmahata9@gmail.com>
2025-01-21 18:02:09 +0530

    UI

--- app/src/main/java/com/groomers/groomersvendor/Common.kt
+++ app/src/main/java/com/groomers/groomersvendor/Common.kt
@@ -1,7 +1,6 @@
 package com.groomers.groomersvendor
 
 import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowInsetsControllerCompat
